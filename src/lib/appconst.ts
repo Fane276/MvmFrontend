9d77f942a1b94a11b014d4984adc54aeaa342ad9
Stefan Cirstea <stefan.sc.sc@gmail.com>
2022-02-04 15:22:45 +0200

    initial commit rename

--- src/lib/appconst.ts
+++ src/lib/appconst.ts
@@ -3,7 +3,7 @@ const AppConsts = {
     defaultAdminUserName: 'admin',
   },
   localization: {
-    defaultLocalizationSourceName: 'MvMangement',
+    defaultLocalizationSourceName: 'MvManagement',
   },
   authorization: {
     encrptedAuthTokenName: 'enc_auth_token',
