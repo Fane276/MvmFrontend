74239c701386dffb10e665eeb61ab1a359e726f7
Stefan Cirstea <stefan.sc.sc@gmail.com>
2022-04-22 18:17:32 +0300

    fix sidemenu mobile

--- src/components/Menu/SidebarMenuWithHeader.jsx
+++ src/components/Menu/SidebarMenuWithHeader.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, useColorModeValue, useMediaQuery } from "@chakra-ui/react";
 import Header from "../Header/Header";
 import SidebarMenu from "./SidebarMenu";
 
@@ -8,6 +8,8 @@ export default function SidebarMenuWithHeader({children}) {
   const [ navSize, setNavSize] = useState("large");
 
   const {collapsed} = useSelector((state)=>state.menu.value);
+  
+  const [isMobileScreen] = useMediaQuery('(max-width:768px)');
 
   useEffect(() => {
     if(collapsed){
@@ -21,7 +23,7 @@ export default function SidebarMenuWithHeader({children}) {
   return (
     <Flex bg={useColorModeValue('gray.100', 'gray.900')}>
       <SidebarMenu />
-      <Box w="100%" minH='100vh' pl={navSize === 'large'? '300px': '100px'}>
+      <Box w="100%" minH='100vh' pl={!isMobileScreen && (navSize === 'large'? '300px': '100px')}>
         <Header/>
         {children}
       </Box>
